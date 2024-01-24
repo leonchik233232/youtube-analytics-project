0ea09fa5cc31711cd4c5940bf44cf5f927fc9fbd
leon <leon.vladykin2288@gmail.com>
2024-01-24 19:17:29 +0500

    dz_1

--- src/channel.py
+++ src/channel.py
@@ -1,30 +1,21 @@
-import requests
+from googleapiclient.discovery import build
+import os
 
 class Channel:
-    """Класс для ютуб-канала"""
-
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+    def __init__(self, channel_id):
+        api_key: str = os.getenv('YT_API_KEY')
         self.channel_id = channel_id
-        self.api_key = "AIzaSyC3adqgbkITK7mjmYywl2kAOp5NuaT3ZXE"
-        pass
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        url = f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={self.channel_id}&key={self.api_key}"
-        response = requests.get(url)
-        data = response.json()
+    def print_info(self):
+        request = self.youtube.channels().list(
+            part='statistics',
+            id=self.channel_id
+        )
+        response = request.execute()
+        statistics = response['items'][0]['statistics']
 
-        channel_data = data["items"][0]
-        title = channel_data["snippet"]["title"]
-        description = channel_data["snippet"]["description"]
-        view_count = channel_data["statistics"]["viewCount"]
-        subscriber_count = channel_data["statistics"]["subscriberCount"]
-        video_count = channel_data["statistics"]["videoCount"]
-
-        print(f"Title: {title}")
-        print(f"Description: {description}")
-        print(f"View Count: {view_count}")
-        print(f"Subscriber Count: {subscriber_count}")
-        print(f"Video Count: {video_count}")
-        pass
+        print('Channel ID:', self.channel_id)
+        print('Subscriber count:', statistics['subscriberCount'])
+        print('View count:', statistics['viewCount'])
+        print('Video count:', statistics['videoCount'])
