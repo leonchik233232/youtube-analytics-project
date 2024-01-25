99c3aaf0c76c00ba34d2a7af26bff43b6555883f
leon <leon.vladykin2288@gmail.com>
2024-01-25 18:15:36 +0500

    dz_1

--- src/channel.py
+++ src/channel.py
@@ -1,13 +1,12 @@
 from googleapiclient.discovery import build
 import os
-
 class Channel:
     def __init__(self, channel_id):
         api_key: str = os.getenv('YT_API_KEY')
         self.channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def print_info(self):
+    def get_info(self):
         request = self.youtube.channels().list(
             part='statistics',
             id=self.channel_id
@@ -15,7 +14,11 @@ class Channel:
         response = request.execute()
         statistics = response['items'][0]['statistics']
 
-        print('Channel ID:', self.channel_id)
-        print('Subscriber count:', statistics['subscriberCount'])
-        print('View count:', statistics['viewCount'])
-        print('Video count:', statistics['videoCount'])
+        info_dict = {
+            'Channel ID': self.channel_id,
+            'Subscriber count': statistics['subscriberCount'],
+            'View count': statistics['viewCount'],
+            'Video count': statistics['videoCount']
+        }
+
+        return info_dict
