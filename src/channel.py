54e99d3d67139b23e6145d096f1a6702de734c18
leon <leon.vladykin2288@gmail.com>
2024-01-27 18:30:05 +0500

    dz_1

--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,7 @@ import json
 class Channel:
     def __init__(self, channel_id):
         self.channel_id = channel_id
-        self.api_key = 'YOUR_API_KEY'
+        self.api_key = 'YT_API_KEY'
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self):
