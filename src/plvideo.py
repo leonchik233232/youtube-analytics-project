4fdbbc80285fffda4b295a4c25adb90db9eb8b6f
leon <leon.vladykin2288@gmail.com>
2024-02-05 19:48:15 +0500

    dz_4

---
+++ src/plvideo.py
@@ -0,0 +1,8 @@
+class PLVideo:
+    def __init__(self, video_id, playlist_id, title, url, view_count, like_count):
+        self.video_id = video_id
+        self.playlist_id = playlist_id
+        self.title = title
+        self.url = url
+        self.view_count = view_count
+        self.like_count = like_count
---
+++ src/video.py
@@ -0,0 +1,7 @@
+class Video:
+    def __init__(self, video_id, title, url, view_count, like_count):
+        self.video_id = video_id
+        self.title = title
+        self.url = url
+        self.view_count = view_count
+        self.like_count = like_count
