eb59cf3480b5c85eab05b3cfbc05dd8a404a081a
leon <leon.vladykin2288@gmail.com>
2024-02-14 14:42:11 +0500

    dz_5

--- src/video.py
+++ src/video.py
@@ -1,7 +1,20 @@
+import requests
+
 class Video:
-    def __init__(self, video_id, title, url, view_count, like_count):
-        self.video_id = video_id
-        self.title = title
-        self.url = url
-        self.view_count = view_count
-        self.like_count = like_count
+    def __init__(self, video_id):
+        try:
+            response = requests.get(f"https://youtube.com/watch?v={video_id}")
+            if response.status_code != 200:
+                raise ValueError("Invalid video ID")
+
+            soup = BeautifulSoup(response.content, "html.parser")
+            self.title = soup.find("title").text
+            self.url = f"https://youtube.com/watch?v={video_id}"
+            self.view_count = int(soup.find("div", class_="view-count").text.split()[0].replace(",", ""))
+            self.like_count = int(soup.find("button", class_="like-button-renderer-like-button").text.split()[0])
+        except (ValueError, AttributeError):
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
